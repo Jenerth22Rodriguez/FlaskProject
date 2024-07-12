803d337a3b43129811d254c0f74fcf8b902c085e
Jenerth Rodriguez <Connerjamestill@gmail.com>
2024-07-12 13:08:41 -0400

    Adding the calculator page

--- venv/app.py
+++ venv/app.py
@@ -5,4 +5,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route("/calculator")
+def calculation():
+    return render_template("calculator.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
